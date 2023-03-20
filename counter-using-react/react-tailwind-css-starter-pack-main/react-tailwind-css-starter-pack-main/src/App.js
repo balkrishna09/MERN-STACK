19752e6e15973d5dacfc149a55a7abd5179aeeda
balkrishna09 <balkrishna4000021@gmail.com>
2023-03-20 10:41:05 +0530

    counter project using react

--- counter-using-react/react-tailwind-css-starter-pack-main/react-tailwind-css-starter-pack-main/src/App.js
+++ counter-using-react/react-tailwind-css-starter-pack-main/react-tailwind-css-starter-pack-main/src/App.js
@@ -18,7 +18,7 @@ function App() {
   }
   return (
     <div className="flex items-center justify-center w-[100vw] h-[100vh] bg-[#344151] flex-col gap-10">
-      <div className="text-[#0398d4] font-medium text-2xl">Increment && Decrement</div>
+      <div className="text-[#0398d4] font-medium text-2xl">Increment & Decrement</div>
       <div className="bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]">
         <button onClick={decreaseHandler} className ="border-r-2 text-center w-20 border-[#bfbfbf] text-5xl">
           -
